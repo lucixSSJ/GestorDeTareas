3ed643bda9fb11eba3755516427c22affac3e75f
lucixSSJ <franzsanzz2@gmail.com>
2025-10-13 12:47:19 -0500

    Correción del proyecto

---
+++ AppTaskFlow/src/main/java/appTaskFlow/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appTaskFlow;
+
+import interfacesGUI.InterfazPrueba;
+
+/**
+ *
+ * @author Luciano
+ */
+public class Main {
+    public static void main(String[] args){
+        InterfazPrueba miVentana = new InterfazPrueba();
+        
+        miVentana.setVisible(true);
+    }
+    
+}
