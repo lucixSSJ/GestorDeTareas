d8314085549d262874a4c610bd7e33ee61eeb9db
lucixSSJ <franzsanzz2@gmail.com>
2025-12-07 22:09:58 -0500

    eliminación

--- AppTaskFlow/src/main/java/gestortareas/dao/ICategoriaDAO.java
+++
@@ -1,8 +0,0 @@
-package gestortareas.dao;
-
-import gestortareas.model.Categoria;
-
-public interface ICategoriaDAO {
-    boolean existCategoria(String nombre);
-    boolean registerCategory(Categoria categoria);
-}
